e892b25a5b2576341814acf7805e9cea90888887
Giovanni Dick <giova19@proton.pm>
2026-07-20 21:37:19 +0100

    Add packaging the app with electron-forge. Patch for building rpm on Fedora 44

---
+++ forge.config.js
@@ -0,0 +1,65 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          maintainer: 'Giovanni Dick',
+          homepage: 'https://github.com/giobuilds'
+        }
+      },
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        background: './assets/dmg-background.png',
+        format: 'ULFO'
+      }
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {
+        options: {}
+      }
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {
+        maintainer: 'Giovanni Dick',
+        homepage: 'https://github.com/giobuilds'
+      },
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
